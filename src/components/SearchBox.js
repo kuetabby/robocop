20114c586349f3404d936b7a13d733cdb8da3b1a
ivan <vansmuhammad@gmail.com>
2019-06-19 21:07:44 +0700

    Prettier config

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const SearchBox = ({searchfield, searchChange}) =>{
-	return(
-		<div className='pa2'>
-			<input 
-			type='search' 
-			placeholder='Search Robot' 
-			className='pa3 ba b--green bg-lightest-blue' 
-			onChange = {searchChange}
-			/>
-		</div>
-	);
-}
+const SearchBox = ({ searchChange }) => {
+  return (
+    <div className="pa2">
+      <input
+        type="search"
+        placeholder="Search Robot"
+        className="pa3 ba b--green bg-lightest-blue"
+        onChange={searchChange}
+      />
+    </div>
+  );
+};
 
 export default SearchBox;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,54 +1,55 @@
-import React, { Component } from 'react';
-import RoboList from '../components/RoboList';
-import SearchBox from '../components/SearchBox';
-import {connect} from 'react-redux'
-import Scroll from '../components/Scroll';
-import {setSearchField, requestRobots} from '../actions'
-import './App.css';
+import React, { useEffect } from "react";
+import RoboList from "../components/RoboList";
+import SearchBox from "../components/SearchBox";
+import { connect } from "react-redux";
+import Scroll from "../components/Scroll";
+import { setSearchField, requestRobots } from "../actions";
+import "./App.css";
 
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    error: state.requestRobots.error,
+    isPending: state.requestRobots.isPending
+  };
+};
 
-const mapStateToProps =  (state) =>{
-	return{
-		searchField: state.searchRobots.searchField,
-		robots: state.requestRobots.robots,
-		error: state.requestRobots.error,
-		isPending: state.requestRobots.isPending
-	}
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChange: event => dispatch(setSearchField(event.target.value)),
+    onRequestRobot: () => dispatch(requestRobots())
+  };
+};
+
+function App(props) {
+  useEffect(() => {
+    props.onRequestRobot();
+  }, []);
+
+  const { searchField, robots, isPending, onSearchChange } = props;
+  const filteredRobot = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  });
+
+  if (isPending) {
+    return <h1> Loading </h1>;
+  } else {
+    return (
+      <div className="tc">
+        <h1 className="f2"> Robocop </h1>
+
+        <SearchBox searchChange={onSearchChange} />
+
+        <Scroll>
+          <RoboList robots={filteredRobot} />
+        </Scroll>
+      </div>
+    );
+  }
 }
 
-const mapDispatchToProps = (dispatch) =>{
-	return {
-		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
-		onRequestRobot: () => dispatch(requestRobots())
-	}
-}
-class App extends Component{
-componentDidMount(){
-	this.props.onRequestRobot()
-}
-
-	render(){
-	const {searchField, robots, isPending, onSearchChange} = this.props;
-	const filteredRobot = robots.filter(robot =>{
-	return robot.name.toLowerCase().includes(searchField.toLowerCase())
-	})
-
-		if(isPending){
-			return <h1> Loading </h1>
-		}else{ 
-		return(
-		<div className='tc'>
-			<h1 className='f2'> Robocop </h1>
-
-			<SearchBox searchChange={onSearchChange}/>
-
-			<Scroll>
-				<RoboList robots={filteredRobot} />
-			</Scroll>
-		</div>
-		);
-		}
-	}
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
